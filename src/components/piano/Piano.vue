2af999a000f78ef54cecd42ae15aef8f26b55950
mylanvoos <mavdbt@gmail.com>
2024-10-29 10:55:00 +1300

    Add basic play/pause functionality

--- src/components/piano/Piano.vue
+++ src/components/piano/Piano.vue
@@ -9,8 +9,8 @@
       </p>
       <div class="flex justify-between">
         <MIDIUpload @midiParsed="handleMIDIParsed" />
-        <button @click="play" class="mt-4 bg-slate-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-          Play MIDI!
+        <button @click="togglePlayPause" class="mt-4 bg-slate-500 text-white px-4 py-2 rounded hover:bg-slate-400 hover:outline-none hover:border-none transition-colors">
+          {{ isPlaying ? 'Pause' : 'Play' }}
         </button>
       </div>
 
@@ -42,6 +42,7 @@ const grandPianoSampler = new Tone.Sampler({
   }
 }).toDestination();
 
+const isPlaying = ref<boolean>(false);
 const pressedKeys = ref<Set<string>>(new Set());
 const midiEvents = ref<MidiEvent[]>([]);
 let midiPart: Tone.Part | null = null;
@@ -75,7 +76,7 @@ const stopNote = (note: string): void => {
   }
 };
 
-const play = async (): Promise<void> => {
+const togglePlayPause = async (): Promise<void> => {
   if (!samplerLoaded.value) {
     console.warn('Sampler not loaded yet');
     return;
@@ -87,29 +88,38 @@ const play = async (): Promise<void> => {
     Tone.getTransport().cancel();
     Tone.getTransport().stop();
 
-    if (midiPart) {
-      midiPart.dispose();
-      midiPart = null;
-    }
+    if (isPlaying.value) {
+      // pause the transport if currently playing
+      Tone.getTransport().pause();
+      isPlaying.value = false;
+    } else {
+      Tone.getTransport().stop();
+      Tone.getTransport().cancel();
 
-    midiPart = new Tone.Part((time, event: MidiEvent) => {
-      if (event.type === 'noteOn') {
-        Tone.getTransport().scheduleOnce(() => playNote(event.note), time);
-      } else if (event.type === 'noteOff') {
-        Tone.getTransport().scheduleOnce(() => stopNote(event.note), time);
+      if (midiPart) {
+        midiPart.dispose();
+        midiPart = null;
       }
-    }, midiEvents.value).start(0);
 
-    Tone.getTransport().bpm.value = 120;
+      midiPart = new Tone.Part((time: number, event: MidiEvent) => {
+        console.log(time);
+        if (event.type === 'noteOn') {
+          playNote(event.note);
+        } else if (event.type === 'noteOff') {
+          stopNote(event.note);
+        }
+      }, midiEvents.value).start(0);
 
-    Tone.getTransport().start();
+      Tone.getTransport().bpm.value = 120;
+
+      Tone.getTransport().start();
+      isPlaying.value = true;
+    }
   } catch (error) {
     console.error('Error playing MIDI:', error);
   }
 };
 
-
-// Clean up when component is unmounted
 onMounted(() => {
   return () => {
     Tone.getTransport().stop();
