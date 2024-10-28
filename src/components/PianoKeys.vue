aead5e437b5922c39ddbf0d73b68bcb87bb637dd
mylanvoos <mavdbt@gmail.com>
2024-10-28 16:57:12 +1300

    Create separate component PianoKeys

---
+++ src/components/PianoKeys.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="piano">
+    <div v-for="node in fullKeyboard" :key="node.key">
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+interface Note {
+  key: string;
+  isSharp: boolean;
+  position: number;
+}
+
+export const fullKeyboard: Note[] = [
+  { key: 'C', isSharp: false, position: 0 },
+  { key: 'C#', isSharp: true, position: 7 },
+  { key: 'D', isSharp: false, position: 14 },
+  { key: 'D#', isSharp: true, position: 21 },
+  { key: 'E', isSharp: false, position: 28 },
+  { key: 'F', isSharp: false, position: 42 },
+  { key: 'F#', isSharp: true, position: 49 },
+  { key: 'G', isSharp: false, position: 56 },
+  { key: 'G#', isSharp: true, position: 63 },
+  { key: 'A', isSharp: false, position: 70 },
+  { key: 'A#', isSharp: true, position: 77 },
+  { key: 'B', isSharp: false, position: 84 }
+];
+</script>
+
